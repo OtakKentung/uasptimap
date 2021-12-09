5a9e3614004024c20a474f2d960dcf665b37c9ef
OtakKentung <95601654+OtakKentung@users.noreply.github.com>
2021-12-09 23:04:55 +0700

    Search place in map 2,3,4
    search place di  directions, satelite ama navigation

--- src/pages/Map2/Map2.js
+++ src/pages/Map2/Map2.js
@@ -10,9 +10,10 @@ function Map2() {
     "esri/views/MapView",
     "esri/widgets/Directions",
     "esri/config",
-    "esri/widgets/Track", "esri/Graphic"], {
+    "esri/widgets/Track", "esri/Graphic",
+    "esri/rest/locator"], {
     css: true
-  }).then(([Map, MapView, Directions, esriConfig, Track, Graphic]) => {
+  }).then(([Map, MapView, Directions, esriConfig, Track, Graphic, locator]) => {
     esriConfig.apiKey = "AAPK646fc7baa89c4b608eb58b54dbbbe1066JR7fGrtnfyFPoTQnl4Z7yp0rUqUJMjQUrurVPBVnBAt75VOFZL-g-La4SwrdiZf";
 
     const map = new Map({
@@ -52,6 +53,69 @@ function Map2() {
       });
 
     view.ui.add(track, "top-left");
+
+    const places = ["Choose a place type...", "Parks and Outdoors", "Coffee shop", "Gas station", "Food", "Hotel"];
+    const select = document.createElement("select", "");
+    select.setAttribute("class", "esri-widget esri-select");
+    select.setAttribute("style", "width: 175px; font-family: 'Avenir Next W00'; font-size: 1em");
+
+    places.forEach(function (p) {
+      const option = document.createElement("option");
+      option.value = p;
+      option.innerHTML = p;
+      select.appendChild(option);
+    });
+
+    view.ui.add(select, "top-right");
+
+    const locatorUrl = "http://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+    function findPlaces(category, pt) {
+      locator.addressToLocations(locatorUrl, {
+        location: pt,
+        categories: [category],
+        maxLocations: 25,
+        outFields: ["Place_addr", "PlaceName"]
+      })
+
+        .then(function (results) {
+          view.popup.close();
+          view.graphics.removeAll();
+
+          results.forEach(function (result) {
+            view.graphics.add(
+              new Graphic({
+                attributes: result.attributes,  // Data attributes returned
+                geometry: result.location, // Point returned
+                symbol: {
+                  type: "simple-marker",
+                  color: "#000000",
+                  size: "12px",
+                  outline: {
+                    color: "#ffffff",
+                    width: "2px"
+                  }
+                },
+
+                popupTemplate: {
+                  title: "{PlaceName}", // Data attribute names
+                  content: "{Place_addr}"
+                }
+              }));
+          });
+
+        });
+
+    }
+    view.watch("stationary", function (val) {
+      if (val) {
+        findPlaces(select.value, view.center);
+      }
+    });
+
+    select.addEventListener('change', function (event) {
+      findPlaces(event.target.value, view.center);
+    });
+
   });
   return (
     <div style={{ height: 800 }} ref={viewDiv}></div>
--- src/pages/Map3/Map3.js
+++ src/pages/Map3/Map3.js
@@ -9,9 +9,9 @@ function Map3() {
             let view;
             let track;
 
-            loadModules(["esri/config", "esri/views/MapView", "esri/WebMap", "esri/widgets/Track", "esri/Graphic"], {
+            loadModules(["esri/config", "esri/views/MapView", "esri/WebMap", "esri/widgets/Track", "esri/Graphic", "esri/rest/locator"], {
                 css: true
-            }).then(([esriConfig, MapView, WebMap, Track, Graphic]) => {
+            }).then(([esriConfig, MapView, WebMap, Track, Graphic, locator]) => {
                 esriConfig.apiKey = "AAPK646fc7baa89c4b608eb58b54dbbbe1066JR7fGrtnfyFPoTQnl4Z7yp0rUqUJMjQUrurVPBVnBAt75VOFZL-g-La4SwrdiZf";
 
                 const map = new WebMap({
@@ -41,6 +41,68 @@ function Map3() {
                     useHeadingEnabled: false
                 });
                 view.ui.add(track, "top-left");
+
+                const places = ["Choose a place type...", "Parks and Outdoors", "Coffee shop", "Gas station", "Food", "Hotel"];
+                const select = document.createElement("select", "");
+                select.setAttribute("class", "esri-widget esri-select");
+                select.setAttribute("style", "width: 175px; font-family: 'Avenir Next W00'; font-size: 1em");
+
+                places.forEach(function (p) {
+                    const option = document.createElement("option");
+                    option.value = p;
+                    option.innerHTML = p;
+                    select.appendChild(option);
+                });
+
+                view.ui.add(select, "top-right");
+
+                const locatorUrl = "http://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer";
+                function findPlaces(category, pt) {
+                    locator.addressToLocations(locatorUrl, {
+                        location: pt,
+                        categories: [category],
+                        maxLocations: 25,
+                        outFields: ["Place_addr", "PlaceName"]
+                    })
+
+                        .then(function (results) {
+                            view.popup.close();
+                            view.graphics.removeAll();
+
+                            results.forEach(function (result) {
+                                view.graphics.add(
+                                    new Graphic({
+                                        attributes: result.attributes,  // Data attributes returned
+                                        geometry: result.location, // Point returned
+                                        symbol: {
+                                            type: "simple-marker",
+                                            color: "#000000",
+                                            size: "12px",
+                                            outline: {
+                                                color: "#ffffff",
+                                                width: "2px"
+                                            }
+                                        },
+
+                                        popupTemplate: {
+                                            title: "{PlaceName}", // Data attribute names
+                                            content: "{Place_addr}"
+                                        }
+                                    }));
+                            });
+
+                        });
+
+                }
+                view.watch("stationary", function (val) {
+                    if (val) {
+                        findPlaces(select.value, view.center);
+                    }
+                });
+
+                select.addEventListener('change', function (event) {
+                    findPlaces(event.target.value, view.center);
+                });
             })
             return () => {
                 //close the map view
