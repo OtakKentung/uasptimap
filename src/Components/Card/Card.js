b2d16a8b9557c1bb1978e82dc338c9fedfe5374e
unknown <antoniuskevin15@gmail.com>
2021-12-12 23:15:53 +0700

    semoga bisa

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -7,36 +7,44 @@ import fotoVallen from './fotoVallen.jpg';
 function Card() {
     return (
         <div className="container">
-            <div className="card">
-                <div className="imgBx">
-                    <img src={fotoAnton} alt=""></img>
-                </div>
-                <div className="content">
-                    <h3>Antonius Kevin</h3>
-                    <h5>00000045444</h5>
-                    <p>CSS, Navbar, Directions, Find Address, 3D Globe</p>
-                </div>
-            </div>
-            <div className="card">
-                <div className="imgBx">
-                    <img src={fotoIwa} alt=""></img>
-                </div>
-                <div className="content">
-                    <h3>Vanness Iwata</h3>
-                    <h5>00000046190</h5>
-                    <p>React, Map, Globe, Search Place</p>
+            <div className='column'>
+                <div className="card">
+                    <div className="imgBx">
+                        <img src={fotoAnton} alt=""></img>
+                    </div>
+                    <div className="content">
+                        <h3>Antonius Kevin</h3>
+                        <h5>00000045444</h5>
+                        <p>CSS, Navbar, Directions, Find Address, 3D Globe</p>
+                    </div>
                 </div>
             </div>
-            <div className="card">
-                <div className="imgBx">
-                    <img src={fotoVallen} alt=""></img>
-                </div>
-                <div className="content">
-                    <h3>Vallencius Gavriel</h3>
-                    <h5>00000045651</h5>
-                    <p>Gallery Maps, Map, Satellite</p>
+            <div className='column'>
+                <div className="card">
+                    <div className="imgBx">
+                        <img src={fotoIwa} alt=""></img>
+                    </div>
+                    <div className="content">
+                        <h3>Vanness Iwata</h3>
+                        <h5>00000046190</h5>
+                        <p>React, Map, Globe, Search Place</p>
+                    </div>
                 </div>
             </div>
+            
+            <div className='column'>
+                <div className="card">
+                    <div className="imgBx">
+                        <img src={fotoVallen} alt=""></img>
+                    </div>
+                    <div className="content">
+                        <h3>Vallencius Gavriel</h3>
+                        <h5>00000045651</h5>
+                        <p>Gallery Maps, Map, Satellite</p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     );
 }
